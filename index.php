1ce1f3b300d24c2cca66998462d61a6c5e9ca34e
Toast Penis <toastpenis@hushmail.com>
2011-01-10 22:41:22 -0800

    Updated input page, centralized text, and adjusted proportions

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <html>
 <head>
 <title>Operation Celebration's CopyPasta Machine!</title>
+<style type="text/css">
+#inputtext	{
+		border: solid #000000 2px;
+		color: #000000;
+		font-weight: bold;
+		scrolling: none;
+		font-family: Arial;
+		background-image: url('uploads/guyfawkesmask-thumb.gif');
+		background-repeat:no-repeat;
+		background-position:right top;
+		margin-right: 0px;
+		text-align: center;
+		}
+</style>
 </head>
 <body>
 <form action="pdf.php">
+<input type="radio" name="inputmethod" value="template" selected="selected"> <b>Fill in this template</b>:<br />
 Location: <input type="text" name="location"><br />
 Date/time: <input type="text" name="date"><br />
+<input type="radio" name="inputmethod" value="textarea"> <b>Or write your own</b>: <small><i>No more than 7 lines</i></small><br />
+<textarea name="inputtext" id="inputtext" rows="7" cols="49">A Protest in Defense of WikiLeaks and Free Expression!</textarea><br />
 <input type="submit" value="submit [Returns a PDF]" />
 </form>
 </body>
--- pdf.php
+++ pdf.php
@@ -2,17 +2,21 @@
 require('fpdf.php');
 $pdf=new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','',8);
-$rows = 9;
+$pdf->SetFont('Arial','B',10);
+$pdf->SetMargins(0,0,0);
+$rows = 7;
+$input = "WTF! You need to give me some input bud";
+if($_REQUEST["inputmethod"] == "template") {$input = "A Protest in Defense of\nWikiLeaks and Free Expression\n".$_REQUEST['date']."\nat\n".$_REQUEST['location'];}
+if($_REQUEST["inputmethod"] == "textarea") {$input = $_REQUEST['inputtext'];}
 while($rows > 0)
  {
  $rows = $rows-1;
- $pdf->SetXY(10,$rows*30);
- $pdf->MultiCell(0,10,"A protest to save the free and open Internet.\nTake the fight to Real Life\n".$_REQUEST['date']." at ".$_REQUEST['location'],1);
- $pdf->Image("uploads/guyfawkesmask.gif", 70, $rows*30, 25, 30);
- $pdf->SetXY(110,$rows*30);
- $pdf->MultiCell(0,10,"A protest to save the free and open Internet.\nTake the fight to Real Life\n".$_REQUEST['date']." at ".$_REQUEST['location'],1);
- $pdf->Image("uploads/guyfawkesmask.gif", 170, $rows*30, 25, 30);
+ $pdf->SetXY(5,($rows*40)+5);
+ $pdf->MultiCell(100,5,$input,0,'C');
+ $pdf->Image("uploads/guyfawkesmask.gif", 81, ($rows*40)+5, 25, 30);
+ $pdf->SetXY(105,($rows*40)+5);
+ $pdf->MultiCell(100,5,$input,0,'C');
+ $pdf->Image("uploads/guyfawkesmask.gif", 181, ($rows*40)+5, 25, 30);
  }
 $pdf->Output();
 ?>
